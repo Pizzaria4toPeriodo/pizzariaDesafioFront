3f4eedc44a6e45e711bc22519b6741c719513ed1
Luis Rodriguez <luiserr1394@gmail.com>
2023-10-21 14:21:57 -0300

    login e rota

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
+import { IndexComponent } from './layout/index/index/index.component';
+import { FooterComponent } from './layout/footer/footer/footer.component';
+import { HeaderComponent } from './layout/header/header/header.component';
+import { LoginComponent } from './sistema/login/login.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    FooterComponent,
+    HeaderComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
