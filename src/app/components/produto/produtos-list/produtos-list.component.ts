0ab43dec63faa4266a1e9e605408489c5af4d0a9
Luis Rodriguez <luiserr1394@gmail.com>
2023-10-21 12:49:03 -0300

    First Commit

---
+++ src/app/components/produto/produtos-list/produtos-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-produtos-list',
+  templateUrl: './produtos-list.component.html',
+  styleUrls: ['./produtos-list.component.scss']
+})
+export class ProdutosListComponent {
+
+}
