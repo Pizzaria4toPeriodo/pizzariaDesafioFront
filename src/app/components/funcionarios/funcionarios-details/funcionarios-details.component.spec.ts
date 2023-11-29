7e4d1fcd8d1db573ea6396af4369c778dc45f4e2
marcelonv011 <marcelonv011@gmail.com>
2023-11-28 21:58:03 -0300

    funcionarios test

--- src/app/components/funcionarios/funcionarios-details/funcionarios-details.component.spec.ts
+++ src/app/components/funcionarios/funcionarios-details/funcionarios-details.component.spec.ts
@@ -1,8 +1,12 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, fakeAsync } from '@angular/core/testing';
 
 import { FuncionariosDetailsComponent } from './funcionarios-details.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
+import { Funcionario } from 'src/app/models/funcionario';
+import { HttpClient } from '@angular/common/http';
+import { of } from 'rxjs';
+import { By } from '@angular/platform-browser';
 
 describe('FuncionariosDetailsComponent', () => {
   let component: FuncionariosDetailsComponent;
@@ -19,7 +23,51 @@ describe('FuncionariosDetailsComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  beforeEach(() => {    
+    let funcionario: Funcionario = new Funcionario();
+    funcionario.id = 1;
+    funcionario.nomeFuncionario = "kaue";
+    funcionario.pedidoList = [];
+    funcionario.username = "kaue123";
+    funcionario.role = "admin";
+    funcionario.token = "asdf";
+    component.funcionario = funcionario;
+  });
+
+  it('criacao do componente', () => {
     expect(component).toBeTruthy();
   });
+
+  it('Teste de 1 @Input - Interpolacao no template', () => {
+    let elemento = fixture.debugElement.query(By.css('input[name="nomeFuncionario"]'));
+    expect(elemento.nativeElement.ngModel).toEqual("kaue");
+  });
+
+  it('Teste no null de @Input 1 - Interpolação no template', () => {
+    let elemento = fixture.debugElement.query(By.css('input[name="nomeFuncionario"]'));
+    expect(elemento.nativeElement.ngModel).not.toBe(null);
+  });
+
+  it('Teste de @Output() retorno', fakeAsync(() => {
+    spyOn(component.retorno, 'emit');
+    component.salvar();
+    expect(component.retorno.emit).toHaveBeenCalled();
+  }));
+
+  beforeEach(() => {
+    let funcionario = new Funcionario();
+    funcionario.id = 1;
+    funcionario.nomeFuncionario = "kaue";
+    funcionario.pedidoList = [];
+    funcionario.username = "kaue123";
+    funcionario.role = "admin";
+    funcionario.token = "asdf";
+  
+    const httpSpy = TestBed.inject(HttpClient)
+    spyOn(httpSpy, 'post').and.returnValue(of(funcionario));
+    spyOn(httpSpy, 'put').and.returnValue(of(funcionario));
+
+    fixture.detectChanges();
+  });
+
 });
--- src/app/components/funcionarios/funcionarios-list/funcionarios-list.component.spec.ts
+++ src/app/components/funcionarios/funcionarios-list/funcionarios-list.component.spec.ts
@@ -3,6 +3,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FuncionariosListComponent } from './funcionarios-list.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
+import { Funcionario } from 'src/app/models/funcionario';
 
 describe('FuncionariosListComponent', () => {
   let component: FuncionariosListComponent;
@@ -21,5 +22,14 @@ describe('FuncionariosListComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('metodo lancamento', () => {
+    const funcionarioMock: Funcionario = { id: 1, nomeFuncionario: 'kaue', pedidoList: [], username: "kaue12", role: "admin", token: "dasdwe" }; 
+
+    spyOn(component.retorno, 'emit');
+    component.lancamento(funcionarioMock);
+
+    expect(component.retorno.emit).toHaveBeenCalledWith(funcionarioMock);
   });
 });
